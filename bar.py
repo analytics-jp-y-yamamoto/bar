6b3fa2bd955fb8da60a9a47f3a8c3920497078e7
analytics-jp-y-yamamoto <129849323+analytics-jp-y-yamamoto@users.noreply.github.com>
2023-04-13 17:28:09 +0900

    Add files via upload

--- bar.py
+++ bar.py
@@ -5,11 +5,11 @@ import japanize_matplotlib
 
 # ページのタイトル設定
 st.set_page_config(
-    page_title="bar",
+    page_title = "bar",
 )
 
 # csv読み込み
-df0 = pd.read_csv('test.csv', index_col=0)
+df0 = pd.read_csv('test.csv', index_col = 0)
 
 # セッション情報の初期化
 if "page_id" not in st.session_state:
@@ -23,31 +23,31 @@ hide_style = """
         footer {visibility: hidden;}
         </style>
         """
-st.markdown(hide_style, unsafe_allow_html=True)
+st.markdown(hide_style, unsafe_allow_html = True)
 
 # 最初のページ
 def main_page():
     st.markdown(
         "<h2 style='text-align: center;'>棒グラフ表示</h2>",
-        unsafe_allow_html=True,
+        unsafe_allow_html = True,
     )
 
-    shop_list=st.session_state.df0.columns.values
-    Day_list=st.session_state.df0.index.values
-    shop_list_selector=st.sidebar.selectbox( "ショップ選択",shop_list)
+    shop_list = st.session_state.df0.columns.values
+    day_list = st.session_state.df0.index.values
+    shop_list_selector = st.sidebar.selectbox("ショップ選択", shop_list)
 
-    top5=st.session_state.df0.sort_values(shop_list_selector,ascending=False)[:5][shop_list_selector]
+    top5=st.session_state.df0.sort_values(shop_list_selector, ascending = False)[:5][shop_list_selector]
 
-    fig1, ax1 = plt.subplots(figsize=(6.4,4.8))
+    fig1, ax1 = plt.subplots(figsize = (6.4, 4.8))
 
     ax1.bar(top5.index.values,top5)
     ax1.set_title(shop_list_selector+"店上位5位売り上げ")
     ax1.set_xlabel("売上年月日")
     ax1.set_ylabel("総売上")
 
-    fig2, ax2 = plt.subplots(figsize=(9.0,5.4))
+    fig2, ax2 = plt.subplots(figsize = (9.0, 5.4))
 
-    ax2.bar(st.session_state.df0.index.values,st.session_state.df0[:][shop_list_selector])
+    ax2.bar(st.session_state.df0.index.values, st.session_state.df0[shop_list_selector])
     ax2.set_title(shop_list_selector+"店売り上げ")
     ax2.set_xlabel("売上年月日")
     ax2.set_ylabel("総売上")
